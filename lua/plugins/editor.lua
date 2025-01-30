77c1dad733e7b5399831c8a99dee5d14ccd0b618
Hector Zurga <hjzurga@gmail.com>
2025-01-30 13:18:55 -0500

    Added formatters config

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+    },
+  },
+}
