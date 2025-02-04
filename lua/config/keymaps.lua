192e60d3d12488387631d57a91b1ea0fb8cd625a
Hector Zurga <hjzurga@gmail.com>
2025-02-03 22:08:17 -0500

    added keymaps to improve typing experience

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set_keymap = vim.api.nvim_set_keymap
+
+set_keymap("n", "<leader>o", "o<ESC>", { silent = true, desc = "Add empty line bellow cursor" })
+set_keymap("n", "<leader>O", "O<ESC>", { silent = true, desc = "Add empty line above cursor" })
+set_keymap("i", "<S-CR>", "<ESC>o", { silent = true, desc = "Add empty line bellow cursor" })
+set_keymap("i", "<C-CR>", "<ESC>O", { silent = true, desc = "Add empty line above cursor" })
+
+set_keymap("n", "<leader>ce", "^ggVG", { silent = true, desc = "Select all content in file" })
